1b957b4739370fbb6c2904ee98beee39ee060a9a
Lenon Nascimento <lenoninformatica@gmail.com>
2023-10-22 17:53:48 -0300

    FIX

--- projects/views.py
+++ projects/views.py
@@ -19,41 +19,37 @@ set_view = viewsets.ModelViewSet
 
 
 class ProfileViewSet(set_view):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-
-
-class ProjectViewSet(set_view):
     # defina a queryset
-    queryset = Project.objects.all()
+    queryset = Profile.objects.all()
     # defina a classe do serializer
-    serializer_class = ProjectSerializer
+    serializer_class = ProfileSerializer
 
     def get_permissions(self):
         if self.request.method == "GET":
             return [AllowAny()]
         return [IsAuthenticated()]
 
-        def retrieve(self, request, *args, **kwargs):
-            if self.request.method == "GET":
-                # busque o id do perfil
-                kwargs.get("pk")
-                # crie uma variável para guardar esse perfil
-                profile = self.get_object()
-                context = {"profile": profile}
-                return render(
-                    # passe os parâmetros necessários para o template:
-                    request,
-                    "profile_detail.html",
-                    context
-                    # a requisição, o caminho do template
-                    # e um dict com dados para o template
-                )
-            return super().retrieve(request, *args, **kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        if self.request.method == "GET":
+            # busque o id do perfil
+            kwargs.get("pk")
+            # crie uma variável para guardar esse perfil
+            profile = self.get_object()
+            context = {"profile": profile}
+            return render(
+                # passe os parâmetros necessários para o template:
+                request,
+                "profile_detail.html",
+                context
+                # a requisição, o caminho do template
+                # e um dict com dados para o template
+            )
+        return super().retrieve(request, *args, **kwargs)
 
-        class ProjectViewSet(set_view):
-            queryset = Project.objects.all()
-            serializer_class = ProjectSerializer
+
+class ProjectViewSet(set_view):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
 class CertifyingInstitutionViewSet(set_view):
