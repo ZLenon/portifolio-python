fac93d03b0bac8f8fe1d1f7e71231e8435f970fc
Lenon Nascimento <lenoninformatica@gmail.com>
2023-10-23 11:58:50 -0300

    Fix

--- projects/serializers.py
+++ projects/serializers.py
@@ -41,16 +41,16 @@ class CertifyingInstitutionSerializer(srms):
         fields = ["id", "name", "url", "certificates"]
 
     def create(self, validated_data):
-        certificates_data = validated_data.pop("certificates")
-        new_certifying_institution = CertifyingInstitution.objects.create(
+        data = validated_data.pop("certificates")
+        novo_certificado = CertifyingInstitution.objects.create(
             **validated_data
         )
-        new_certificate = {}
-        for certificate in certificates_data:
-            new_certificate = {
-                "name": certificate["name"],
-                "certifying_institution": new_certifying_institution,
+        certificado = {}
+        for x in data:
+            certificado = {
+                "name": x["name"],
+                "certifying_institution": novo_certificado,
                 "profiles": [],
             }
-            CertificateSerializer().create(new_certificate)
-        return new_certifying_institution
+            CertificateSerializer().create(certificado)
+        return novo_certificado
